adb104e8f878e766fa5f8bc4b0fdeacd387d3b40
Mike <matthews.r.mike@gmail.com>
2024-09-06 21:54:28 +0200

    Add TODOs for patches

--- Patches.cs
+++ Patches.cs
@@ -2,18 +2,22 @@ using HarmonyLib;
 
 namespace DamageUI
 {
-	// Patch model
-	// [HarmonyPatch(typeof(), nameof())]
-	// static class type_method_Patch()
-	// {
-	// 	static void Prefix()
-	// 	{
-	// 		//
-	// 	}
+    // Patch model
+    // [HarmonyPatch(typeof(), nameof())]
+    // static class type_method_Patch()
+    // {
+    // 	static void Prefix()
+    // 	{
+    // 		//
+    // 	}
 
-	// 	static void Postfix()
-	// 	{
-	// 		//
-	// 	}
-	// }
+    // 	static void Postfix()
+    // 	{
+    // 		//
+    // 	}
+    // }
+
+    // TODO : Patch event when a part takes damage / send through the state of the part
+    // TODO : Find the event in which we can plug to decide when we need to spawn
+    // TODo : Patch event when a wheel gets a puncture
 }
